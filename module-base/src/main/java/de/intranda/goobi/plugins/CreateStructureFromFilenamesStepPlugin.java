d6713f0dbe07442529420b388209ce81e88cd21c
Paul Hankiewicz Lopez <paul@intranda.com>
2024-08-19 11:22:37 +0200

    Changed some variables and added a message output into the journal

--- module-base/src/main/java/de/intranda/goobi/plugins/CreateStructureFromFilenamesStepPlugin.java
+++ module-base/src/main/java/de/intranda/goobi/plugins/CreateStructureFromFilenamesStepPlugin.java
@@ -36,10 +36,12 @@ import ugh.dl.Prefs;
 import ugh.dl.Metadata;
 import ugh.dl.ContentFile;
 import ugh.dl.DigitalDocument;
+import de.sub.goobi.helper.Helper;
 import de.sub.goobi.helper.StorageProvider;
 import java.util.List;
 import java.util.TreeMap;
 
+import org.goobi.production.enums.LogType;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginReturnValue;
 import org.goobi.production.enums.PluginType;
@@ -73,8 +75,8 @@ public class CreateStructureFromFilenamesStepPlugin implements IStepPluginVersio
 
 		// read parameters from correct block in configuration file
 		SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
-		structureElementType = myconfig.getString("structureElementType", "default structureElementType");
-		filenameString = myconfig.getString("filenameString", "default filenameString");
+		structureElementType = myconfig.getString("structureElementType", "Chapter");
+		filenameString = myconfig.getString("filenameString", "_backprint");
 		log.info("CreateStructureFromFilenames step plugin initialized");
 	}
 
@@ -132,17 +134,16 @@ public class CreateStructureFromFilenamesStepPlugin implements IStepPluginVersio
 			// Get the directory and print it on the console
 			foldername = process.getImagesOrigDirectory(false);
 
-			// Get the names of the folders and sort them in a list and print the list on
-			// the console
+			// Get the names of the folders and sort them in a list
 			fileList = StorageProvider.getInstance().list(foldername);
 
 			// Go through all filenames in the list
 			for (String fileName : fileList) {
-				int underscoreIndex = fileName.indexOf("_");
+				int separatorIndex = fileName.indexOf(filenameString);
 
-				if (underscoreIndex == -1) {
-					// Get or create a list associated with this modifiedFileName in the TreeMap
-					int pointIndex = fileName.indexOf(".");
+				if (separatorIndex == -1) {
+					// Get or create a list associated with this modifiedFileName in the treeMap
+					int pointIndex = fileName.lastIndexOf(".");
 					String modifiedFileName = fileName.substring(0, pointIndex);
 					//
 					List<String> list = treeMap.get(modifiedFileName);
@@ -152,13 +153,12 @@ public class CreateStructureFromFilenamesStepPlugin implements IStepPluginVersio
 					}
 					list.add(fileName);
 				} else {
-					// Get everything before the first underscore
-					String modifiedFileName = fileName.substring(0, underscoreIndex);
+					// Get everything before the filenameString
+					String modifiedFileName = fileName.substring(0, separatorIndex);
 
 					// Check if the modifiedFileName already exists in the treeMap
 					if (treeMap.containsKey(modifiedFileName)) {
-						// If it exists, add the original fileName to the list associated with
-						// modifiedFileName
+						// If it exists, add the original fileName to the list associated with modifiedFileName
 						treeMap.get(modifiedFileName).add(fileName);
 					} else {
 						// If it doesn't exist, create a new entry with modifiedFileName as the key
@@ -207,7 +207,7 @@ public class CreateStructureFromFilenamesStepPlugin implements IStepPluginVersio
 
 					// Create and add a content file
 					ContentFile cf = new ContentFile();
-					cf.setMimetype("image/tiff"); // Der MIME-Typ sollte auf das tatsächliche Format abgestimmt sein
+					cf.setMimetype("image/tiff");
 					cf.setLocation(imageFile.getAbsolutePath());
 					dsPage.addContentFile(cf);
 
@@ -226,7 +226,10 @@ public class CreateStructureFromFilenamesStepPlugin implements IStepPluginVersio
 
 		log.info("CreateStructureFromFilenames step plugin executed");
 		if (!successful) {
+			Helper.addMessageToProcessJournal(step.getProcessId(), LogType.ERROR, "CreateStructureFromFilenames plugin encountered an error while processing filenames and updating the document structure.");
 			return PluginReturnValue.ERROR;
+		} else {
+			Helper.addMessageToProcessJournal(step.getProcessId(), LogType.INFO, "CreateStructureFromFilenames plugin successfully processed filenames and updated the document structure.");
 		}
 		return PluginReturnValue.FINISH;
 	}
